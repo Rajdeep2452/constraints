f841000e7e1a903283ddc84acaddda3014d81b46
Rajdeep2452 <153487007+Rajdeep2452@users.noreply.github.com>
2023-12-14 13:24:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,3 +6,4 @@ aws_region = 'us-east-1'
 # Specify your table name
 table_name_hcp = 'HCP_Level_Constraint-Quarterly'
 table_name_clc = 'Channel_Level_Constraints-Quarterly'
+table_name_pt = 'Priority_and_Triggers'

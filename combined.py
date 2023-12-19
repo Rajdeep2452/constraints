f363e6ac9e2ef4018bccac5364a60ad4254248fe
Rajdeep2452 <153487007+Rajdeep2452@users.noreply.github.com>
2023-12-19 06:04:16 +0530

    Update combined.py

--- combined.py
+++ combined.py
@@ -14,6 +14,7 @@ table_hcp = dynamodb.Table(table_name_hcp)
 table_clc = dynamodb.Table(table_name_clc)
 table_pt = dynamodb.Table(table_name_pt)
 table_suppression = dynamodb.Table(table_name_suppression)
+table_summary = dynamodb.Table(table_name_summary)
 
 # Default values for each column
 default_values_hcp = {
@@ -297,6 +298,37 @@ class RequestHandler(BaseHTTPRequestHandler):
             items = response.get('Items', [])
             self._send_response(200, items)
 
+        elif self.path == '/Summary':
+            try:
+                # Retrieve data from the DynamoDB table
+                response = table_summary.get_item(Key={'id': 1})
+
+                # Check if data exists
+                if 'Item' in response:
+                    data = response['Item']
+
+                    # Send a response back to the client
+                    self.send_response(200)
+                    self.send_header('Content-type', 'application/json')
+                    self._set_cors_headers()
+                    self.end_headers()
+                    self.wfile.write(json.dumps(data, cls=DecimalEncoder).encode())
+                else:
+                    # Send a response back to the client if data doesn't exist
+                    self.send_response(404)
+                    self.send_header('Content-type', 'application/json')
+                    self._set_cors_headers()
+                    self.end_headers()
+                    self.wfile.write(json.dumps({'error': 'Data not found'}).encode('utf-8'))
+
+            except Exception as e:
+                # Send an error response back to the client
+                self.send_response(500)
+                self.send_header('Content-type', 'application/json')
+                self._set_cors_headers()
+                self.end_headers()
+                self.wfile.write(json.dumps({'error': str(e)}).encode('utf-8'))
+
         else:
             self._send_response(404, {'error': 'Not Found'})
 
@@ -442,6 +474,75 @@ class RequestHandler(BaseHTTPRequestHandler):
                 table_suppression.put_item(Item={**{'id': 1},**post_data})
 
             self._send_response(201, ['Data updated successfully', post_data])
+
+        elif self.path == '/Summary':
+            content_length = int(self.headers['Content-Length'])
+            body = self.rfile.read(content_length)
+            data = json.loads(body.decode('utf-8'))
+
+            try:
+                # Extract values from the request
+                num_rep = data['Num_Rep']
+                calls_recomm = data['Calls_Recomm']
+                rte_recomm = data['RTE_Recomm']
+                insights = data['Insights']
+
+                # Check if data for the given id exists
+                existing_data = table_summary.get_item(Key={'id': 1}).get('Item')
+
+                if existing_data:
+                    # Update existing data
+                    response = table_summary.update_item(
+                        Key={'id': 1},
+                        UpdateExpression='SET Num_HCP = :nh, Num_Rep = :nr, Recomm_Cycle = :rc, Recomm_Date = :rd, '
+                                        'Calls_Recomm = :cr, RTE_Recomm = :rte, Insights = :ins, '
+                                        'Avg_Calls = :ac, Avg_RTE = :art, Avg_Insights = :ai',
+                        ExpressionAttributeValues={
+                            ':nh': data['Num_HCP'],
+                            ':nr': num_rep,
+                            ':rc': data['Recomm_Cycle'],
+                            ':rd': data['Recomm_Date'],
+                            ':cr': calls_recomm,
+                            ':rte': rte_recomm,
+                            ':ins': insights,
+                            ':ac': str(calls_recomm / num_rep),
+                            ':art': str(rte_recomm / num_rep),
+                            ':ai': str(insights / num_rep)
+                        },
+                        ReturnValues='ALL_NEW'
+                    )
+                else:
+                    # Insert new data
+                    response = table_summary.put_item(
+                        Item={
+                            'id': 1,
+                            'Num_HCP': data['Num_HCP'],
+                            'Num_Rep': num_rep,
+                            'Recomm_Cycle': data['Recomm_Cycle'],
+                            'Recomm_Date': data['Recomm_Date'],
+                            'Calls_Recomm': calls_recomm,
+                            'RTE_Recomm': rte_recomm,
+                            'Insights': insights,
+                            'Avg_Calls': str(calls_recomm / num_rep),
+                            'Avg_RTE': str(rte_recomm / num_rep),
+                            'Avg_Insights': str(insights / num_rep)
+                        }
+                    )
+
+                # Send a response back to the client
+                self.send_response(200)
+                self.send_header('Content-type', 'application/json')
+                self._set_cors_headers()
+                self.end_headers()
+                self.wfile.write(json.dumps(["Data Inserted Successfully", data], cls=DecimalEncoder).encode())
+
+            except Exception as e:
+                # Send an error response back to the client
+                self.send_response(500)
+                self.send_header('Content-type', 'application/json')
+                self._set_cors_headers()
+                self.end_headers()
+                self.wfile.write(json.dumps({'error': str(e)}).encode('utf-8'))
 
         else:
             self._send_response(404, {'error': 'Not Found'})
